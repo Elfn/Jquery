dc299e4d064b6e9e01da68e4a61b1be22031679d
elimane.fofana <Lafayette@2020>
2020-09-10 17:01:01 +0000

    new

--- Interaction1 - Draggable/Test/assets/js/script.js
+++ Interaction1 - Draggable/Test/assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
        // $("p, #box").draggable();
         $("#box2").draggable({axis: "x"});//Horizontal direction
         $("#box").draggable({axis: "y"});//Vertical direction
-        $("#box, #box2").draggable({containment: "#main"});//Vertical direction
+        $("#box, #box2").draggable({containment: "#main"});//containment
         //$("#box").draggable();
         
     });
